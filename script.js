778dd80e690fec7bc837bf6ee69c16f8cd7896ed
IT Compliance <compliance@amalitech.org>
2024-06-19 16:31:20 -0700

    display advice on button click

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-// fetch api 
-fetch('https://api.adviceslip.com/advice').then(response => {
-    // console.log('resolved:', response)
-    return response.json()
-}).then(data => {
-    console.log(data)
-}).catch(err => {
-    console.log('rejected:', err)
-})
+const adviceNumber = document.querySelector('.advice-title')
+const adviceContent = document.querySelector('.advice-content')
+const btnEl = document.querySelector('.click-btn')
+
+
+// fetch api
+const fetchAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => {
+        if (response.status !== 200){
+            throw new Error('Unable to retrieve data')
+        }
+        return response.json()
+    }).then(result => {
+        const data = result.slip
+        let adviceId = `Advice #${data.id}`
+        let advice = `"${data.advice}"`
+
+        adviceNumber.textContent = adviceId
+        adviceContent.textContent = advice
+        console.log(data)
+    }).catch(err => {
+        console.log('rejected:', err)
+    })
+}
+
+btnEl.addEventListener('click', fetchAdvice)
+
 
