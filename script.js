e0d763259ee44a146345f26af5c1e160ed685d94
IT Compliance <compliance@amalitech.org>
2024-06-19 15:14:07 -0700

    fetching api

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// fetch api 
+fetch('https://api.adviceslip.com/advice').then(response => {
+    // console.log('resolved:', response)
+    return response.json()
+}).then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log('rejected:', err)
+})
 
